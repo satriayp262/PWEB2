2369a0f6a557261fa886a2b6713b4e7dbc1994c1
satriayp262 <127200227+satriayp262@users.noreply.github.com>
2023-10-16 13:43:50 +0700

    Update function.php

--- JOBSHEET 1/function.php
+++ JOBSHEET 1/function.php
@@ -7,9 +7,9 @@
 </head>
 <body>
     <form method="post" action="">
-        Masukan Panjang persegi : <input type="number" name="panjang"><br>
-        Masukan Lebar persegi : <input type="number" name="lebar"><br>
-        Masukan Jari-Jari lingkaran : <input type="number" name="jari"><br>
+        Masukan Panjang persegi : <input type="text" name="panjang"><br><br>
+        Masukan Lebar persegi : <input type="text" name="lebar"><br><br>
+        Masukan Jari-Jari lingkaran : <input type="text" name="jari"><br>
         <input type="submit" name="submit" value="Submit">
     </form>
     <?php
@@ -28,7 +28,7 @@
     function luasLingkaran($r){
         return  3.14 * $r * $r;
     }
-    
+
     echo "Luas Persegi = ",luasPersegi($p, $l);
     echo "</br>";
     echo "Luas Lingkaran = ",luasLingkaran($r);
