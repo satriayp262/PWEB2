3cd89d000c6965e21a9304996e27c79bc75bece3
satriayp262 <127200227+satriayp262@users.noreply.github.com>
2023-10-16 08:55:19 +0700

    Create variables.php

---
+++ JOBSHEET 1/variables.php
@@ -0,0 +1,18 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Variables</title>
+</head>
+<body>
+    <?php
+    $integer=25;
+    $string="Praktikum Web 2";
+    $bolean=true;
+
+    echo "Ini contoh integer : $integer <br/>";
+    echo "Ini contoh string : $string <br/>";
+    echo "Ini contoh bolean : $bolean <br/>";
+    ?>
+</body>
+</html>
