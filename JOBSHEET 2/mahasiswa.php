de259a65f07425e31191a71f03560c7c958bb61d
satriayp262 <127200227+satriayp262@users.noreply.github.com>
2023-10-16 23:07:27 +0700

    Update mahasiswa.php

--- JOBSHEET 2/mahasiswa.php
+++ JOBSHEET 2/mahasiswa.php
@@ -14,7 +14,7 @@ class mahasiswa{
 
     //method untuk menampilkan alamat
     function tampil_alamat(){
-    //isi method
+        return "Alamat saya adalah jl slamet sidanegara cilacap tengah";
     }
 }
 
@@ -27,7 +27,7 @@ class dosen{
 
     //method untuk menampilkan nidn
     function tampil_nidn(){
-        //isi method
+        return "NIDN saya adalah 220302093";
     }
 
     //method untuk menampilkan nama
@@ -37,18 +37,29 @@ class dosen{
 
     //method untuk menampilkan prodi
     function tampil_prodi(){
-        //isi method
+        return "Prodi saya adalah Tenik Informatika";
     }
 }    
 
 //membuat objek nama_mahasiswa
 $nama_mahasiswa=new mahasiswa();
+$alamat_mahasiswa=new mahasiswa();
 
 //menampilkan objek ke layar
-//echo $nama_mahasiswa->tampil_nama();
+echo $nama_mahasiswa->tampil_nama();
+echo "</br>";
+echo $alamat_mahasiswa->tampil_alamat();
+echo "</br>";
+echo "</br>";
 
 //membuat objek nama_dosen
 $nama_dosen=new dosen();
+$nidn_dosen=new dosen();
+$prodi_dosen=new dosen();
 
 //menampilkan objek ke layar
 echo $nama_dosen->tampil_namadosen();
+echo "</br>";
+echo $nidn_dosen->tampil_nidn();
+echo "</br>";
+echo $prodi_dosen->tampil_prodi();
