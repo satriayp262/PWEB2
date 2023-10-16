4f0bb481c8a053d77d01e9f6ae624fc4f762d5dd
satriayp262 <127200227+satriayp262@users.noreply.github.com>
2023-10-16 10:46:16 +0700

    Update branching.php

--- JOBSHEET 1/branching.php
+++ JOBSHEET 1/branching.php
@@ -5,15 +5,23 @@
     <title>Branching</title>
 </head>
 <body>
+    <h1>Pengecekan Bilangan</h1>
+    <form method="post">
+        Masukan Bilangan : <input type="text" name="bilangan">
+        <input type="submit" value="cek">
+    </form>
+
     <?php
-    $x = 0;
-    echo "Nilai X = $x <br/>";
-    if ($x > 0) {
-        echo "$x adalah Bilangan positif";}
-    elseif($x < 0){
-        echo "$x adalah Bilangan Negatif";}
-    else {
-        echo "$x adalah Bilangan Nol";
+    if ($_SERVER["REQUEST_METHOD"] == "POST"){
+        $x = $_POST["bilangan"];
+    
+        if ($x > 0) {
+            echo "$x adalah Bilangan positif";}
+        elseif($x < 0){
+            echo "$x adalah Bilangan Negatif";}
+        else {
+            echo "$x adalah Bilangan Nol";
+    }
     }
     ?>
 </body>
