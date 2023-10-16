e233a5747cf05c4a46addd656e4b7c4eaec5020f
satriayp262 <127200227+satriayp262@users.noreply.github.com>
2023-10-16 19:00:52 +0700

    Update inheritance.php

--- JOBSHEET 2/inheritance.php
+++ JOBSHEET 2/inheritance.php
@@ -1,32 +1,37 @@
 <?php
 
-class manusia{
+class Manusia { 
     public $nama_saya;
     private $nim_saya;
 
     public function panggil_nama($saya){
         $this->nama_saya = $saya;
     }
-    
-}
-
-//class turunan dari manusia
-class mahasiswa extends Manusia
-{
-    public $nama_mahasiswa;
-
-    function panggil_mahasiswa($mahasiswa)
-    {
-        $this->nama_mahasiswa = $mahasiswa;
+    function panggil_nim($nim){
+        $this->nim_saya = $nim;
     }
 }
 
-//instansiasi class mahasiswa
-$informatika= new mahasiswa();
+// Class turunan dari Manusia
+class Mahasiswa extends Manusia { 
+    public $nama_mahasiswa;
 
-$informatika->panggil_nama("Satria Yudha");
+    function panggil_mahasiswa($mahasiswa) {
+        $this->nama_mahasiswa = $mahasiswa;
+    }
+    function panggil_nim($nim) { 
+        $this->nim_saya = $nim; 
+    }
+}
+
+// Instansiasi class Mahasiswa
+$informatika = new Mahasiswa(); 
+
+$informatika->panggil_nama("Satria Yudha"); 
 $informatika->panggil_mahasiswa("Pangrangau");
+$informatika->panggil_nim("220302093");
 
-//menampilkan isi property
-echo "Nama depan saya : " . $informatika->nama_saya . "<br/>";
-echo "Nama belakang saya : " . $informatika->nama_mahasiswa;
+// Menampilkan isi property
+echo "Nama depan saya : " . $informatika->nama_saya . "<br/>"; 
+echo "Nama belakang saya : " . $informatika->nama_mahasiswa . "<br/>";
+echo "NIM saya : " . $informatika->nim_saya;
