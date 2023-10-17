be12df3e3b727b0e961cc48d7cfb1970720a31b0
satriayp262 <127200227+satriayp262@users.noreply.github.com>
2023-10-17 14:14:57 +0700

    Update databases.php

--- JOBSHEET 3/app/classes/databases.php
+++ JOBSHEET 3/app/classes/databases.php
@@ -5,17 +5,39 @@ class database{
     var $username="root";
     var $password="";
     var $db="akademik";
+    var $koneksi;
 
     function __construct()
     {
-        $koneksi mysqli_connect($this->host,$this->username,$this->password,$this->db);
+        $this->koneksi = mysqli_connect($this->host,$this->username,$this->password,$this->db);
     }
 
     function tampil_mahasiswa(){
+        $hasil=array();
         $data=mysqli_query($this->koneksi,"select * from mahasiswa");
-        while($row=mysqli_fetch_array($data)){
-            $hasil=$d;
+        while($d = mysqli_fetch_array($data)){
+            $hasil[] = $d;
          }
          return $hasil;
     }
+
+    function tambah_mhs($nim,$nama,$alamat){
+        mysqli_query($this->koneksi,"insert into mahasiswa (nim,nama,alamat) values('$nim','$nama','$alamat')");
+    }
+
+    function edit($id){
+        $data = mysqli_query($this->koneksi,"select * from mahasiswa where id='$id'");
+        while($d = mysqli_fetch_array($data)){
+            $hasil[] = $d;
+        }
+        return $hasil;
+    }
+
+    function update($id, $nim, $nama, $alamat){
+        mysqli_query($this->koneksi,"update mahasiswa set nim='$nim',nama='$nama' ,alamat='$alamat' where id='$id'");
+    }  
+
+    function hapus($id){
+        mysqli_query($this->koneksi,"delete from mahasiswa where id='$id'");
+    }    
 }
