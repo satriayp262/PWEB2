ae918f5038fde2bd1f8d6c967444682b1074a1e1
satriayp262 <127200227+satriayp262@users.noreply.github.com>
2023-10-17 14:15:07 +0700

    Update tampil_mhs.php

--- JOBSHEET 3/app/views/tampil_mhs.php
+++ JOBSHEET 3/app/views/tampil_mhs.php
@@ -1,21 +1,50 @@
 <?php
 //menampilkan class database
-include '../classes/databases.php'
+include '../classes/databases.php';
 //instansiasi class database
 $db = new database;
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Mahasiswa</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="../../public/style.css">
+</head>
+<body>
 
 <h3>Data Mahasiswa</h3>
-<a href="input_mhs.php">Tambah Mahasiswa</a>
-<table border="1">
+<a href="input_mhs.php" class="btn btn-primary">Tambah Mahasiswa</a>
+<table class="table table-striped" style="width: 70%;">
     <tr>
         <th>No</th>
         <th>NIM</th>
         <th>Nama</th>
         <th>Alamat</th>
+        <th>Opsi</th>
     </tr>
     <?php 
-    $no=1;
-    foreach($db->tampil_mahasiswa() as $x) {
+    $no = 1;
+    foreach($db->tampil_mahasiswa() as $x){
+    ?>
+        <tr>
+            <td><?php echo $no++ ?></td>
+            <td><?php echo $x['nim'] ?></td>
+            <td><?php echo $x['nama'] ?></td>
+            <td><?php echo $x['alamat'] ?></td>
+            <td>
+            <a href="edit_mhs.php?id=<?php echo $x['id']; ?>&aksi=edit" class="btn btn-warning">edit</a>
+            <a href="proses_mhs.php?id=<?php echo $x['id']; ?>&aksi=hapus" class="btn btn-danger">hapus</a>
+            </td>
+        </tr>
+    <?php
+    }
     ?>
 </table>
+
+    
+</body>
+</html>
+<script src=”https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js”></script>
