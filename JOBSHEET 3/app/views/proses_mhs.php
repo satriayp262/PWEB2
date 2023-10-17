ab09ddb69c00bbb217f50afa7cbf994e743ed827
satriayp262 <127200227+satriayp262@users.noreply.github.com>
2023-10-17 14:15:22 +0700

    Create proses_mhs.php

---
+++ JOBSHEET 3/app/views/proses_mhs.php
@@ -0,0 +1,16 @@
+<?php
+include '../classes/databases.php';
+$db = new database();
+
+$aksi= $_GET['aksi'];
+if($aksi=="tambah"){
+    $db->tambah_mhs($_POST['nim'], $_POST['nama'], $_POST['alamat']);
+    header("location:tampil_mhs.php");
+}elseif($aksi=="update"){
+    $db->update($_POST['id'],$_POST['nim'], $_POST['nama'], $_POST['alamat']);
+    header("location:tampil_mhs.php");
+}elseif($aksi=="hapus"){
+    $db->hapus($_GET['id']);
+    header("location:tampil_mhs.php");
+}
+   
