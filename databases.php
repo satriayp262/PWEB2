ffe4b97e4626c7846d472ca7ddf5b987d118ae63
satriayp262 <127200227+satriayp262@users.noreply.github.com>
2023-12-06 19:52:28 +0700

    progress

---
+++ databases.php
@@ -0,0 +1,24 @@
+<?php
+
+class Databases {
+    var $host = "localhost";
+    var $username = "root";
+    var $password = "";
+    var $db = "rumah_sakit";
+    var $koneksi;
+
+    function __construct(){
+        $this->koneksi = mysqli_connect($this->host, $this->username, $this->password, $this->db, $this->koneksi);
+    }
+
+    function tampil_pasien(){
+        $data = mysqli_query($this->koneksi, "SELECT * FROM rumah_sakit");
+        while ($d = mysqli_fetch_array($data)) {
+            $hasil[] = $d;
+    }
+    return $hasil;
+    }
+
+    function tambah_pasien($nama,$jenis_kelamin, $alamat){
+        
+}
